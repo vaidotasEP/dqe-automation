d6878643c8613ed84f160efdc6f89caebaa5dfcd
SRUOV001 <Vaidotas.Sruogis.-ND@disney.com>
2025-11-21 21:43:15 +0200

    add: updated conftest.py

--- PyTest DQ Framework/tests/conftest.py
+++ PyTest DQ Framework/tests/conftest.py
@@ -5,25 +5,51 @@ from src.connectors.file_system.parquet_reader import ParquetReader
 
 def pytest_addoption(parser):
     parser.addoption("--db_host", action="store", default="localhost", help="Database host")
+    parser.addoption("--db_port", action="store", default="5434", help="Database port")
+    parser.addoption("--db_name", action="store", default="mydatabase", help="Database name")
+    parser.addoption("--db_user", action="store", default="myuser", help="Database user")
+    parser.addoption("--db_password", action="store", default="mypassword", help="Database password")
 
 def pytest_configure(config):
     """
     Validates that all required command-line options are provided.
     """
     required_options = [
-        "--db_user", "--db_password"
-    ]
+        "--db_host", "--db_port", "--db_name"
+    ]  # , "--db_user=", "--db_password"
     for option in required_options:
         if not config.getoption(option):
             pytest.fail(f"Missing required option: {option}")
 
 @pytest.fixture(scope='session')
 def db_connection(request):
-    ...
+    db_host = request.config.getoption("--db_host")
+    db_name = request.config.getoption("--db_name")
+    db_port = request.config.getoption("--db_port")
+    db_user = request.config.getoption("--db_user")
+    db_password = request.config.getoption("--db_password")
     try:
         with PostgresConnectorContextManager(...) as db_connector:
             yield db_connector
     except Exception as e:
         pytest.fail(f"Failed to initialize PostgresConnectorContextManager: {e}")
 
-...
+@pytest.fixture(scope='session')
+def parquet_reader(request):
+    try:
+        reader = ParquetReader()
+        yield reader
+    except Exception as e:
+        pytest.fail(f"Failed to initialize ParquetReader: {e}")
+    finally:
+        del reader
+
+@pytest.fixture(scope='session')
+def data_quality_library():
+    try:
+        data_quality_library = DataQualityLibrary()
+        yield data_quality_library
+    except Exception as e:
+        pytest.fail(f"Failed to initialize DataQualityLibrary: {e}")
+    finally:
+        del data_quality_library
