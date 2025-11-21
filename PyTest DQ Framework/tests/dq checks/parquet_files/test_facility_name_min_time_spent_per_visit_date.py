354cec882da9d3dd860a6f66fd6d120394015add
SRUOV001 <Vaidotas.Sruogis.-ND@disney.com>
2025-11-21 21:44:13 +0200

    add: updated test case - test_facility_name_min_time_spent_per_visit_date.py

--- PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
+++ PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
@@ -5,3 +5,29 @@ Author(s): Name Surname
 """
 
 import pytest
+
+@pytest.mark.parquet_data
+@pytest.mark.smoke
+@pytest.mark.facility_name_min_time_spent_per_visit_date
+def test_check_dataset_is_not_empty(target_data, data_quality_library):
+    data_quality_library.check_dataset_is_not_empty(target_data)
+
+@pytest.mark.parquet_data
+@pytest.mark.facility_name_min_time_spent_per_visit_date
+def test_check_data_completeness(source_data, target_data, data_quality_library):
+    data_quality_library.check_data_completeness(source_data, target_data)
+
+@pytest.mark.parquet_data
+@pytest.mark.facility_name_min_time_spent_per_visit_date
+def test_check_count(source_data, target_data, data_quality_library):
+    data_quality_library.check_count(source_data, target_data)
+
+@pytest.mark.parquet_data
+@pytest.mark.facility_name_min_time_spent_per_visit_date
+def test_check_uniqueness(target_data, data_quality_library):
+    data_quality_library.check_duplicates(target_data)
+
+@pytest.mark.parquet_data
+@pytest.mark.facility_name_min_time_spent_per_visit_date
+def test_check_not_null_values(target_data, data_quality_library):
+    data_quality_library.check_not_null_values(target_data, ['facility_name', 'visit_date', 'min_time_spent'])
