887c5b2e5b4f7e8453e889b57b7622c69709313c
SRUOV001 <Vaidotas.Sruogis.-ND@disney.com>
2025-11-22 03:05:28 +0200

    add: update tests - test_facility_name_min_time_spent_per_visit_date.py

--- PyTestDQFramework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
+++ PyTestDQFramework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
@@ -6,6 +6,35 @@ Author(s): Name Surname
 
 import pytest
 
+@pytest.fixture(scope='module')
+def source_data(db_connection):
+    source_query = """
+    SELECT
+        f.facility_name,
+        CAST(v.visit_timestamp AS DATE) AS visit_date,
+        MIN(v.duration_minutes) AS avg_time_spent
+    FROM visits v
+    JOIN facilities f
+        ON f.id = v.facility_id
+    WHERE v.duration_minutes IS NOT NULL
+    GROUP BY
+        f.facility_name,
+        CAST(v.visit_timestamp AS DATE)
+    ORDER BY
+        f.facility_name,
+        visit_date;
+    """
+    source_data  = db_connection.get_data_sql(source_query)
+    return source_data
+
+
+@pytest.fixture(scope='module')
+def target_data(parquet_reader):
+    target_path = '/parquet_data/facility_name_min_time_spent_per_visit_date/'
+    target_data = parquet_reader.process(target_path)
+    return target_data
+
+
 @pytest.mark.parquet_data
 @pytest.mark.smoke
 @pytest.mark.facility_name_min_time_spent_per_visit_date
@@ -13,21 +42,25 @@ def test_check_dataset_is_not_empty(target_data, data_quality_library):
     data_quality_library.check_dataset_is_not_empty(target_data)
 
 @pytest.mark.parquet_data
+@pytest.mark.data_completeness
 @pytest.mark.facility_name_min_time_spent_per_visit_date
 def test_check_data_completeness(source_data, target_data, data_quality_library):
     data_quality_library.check_data_completeness(source_data, target_data)
 
 @pytest.mark.parquet_data
+@pytest.mark.data_completeness
 @pytest.mark.facility_name_min_time_spent_per_visit_date
 def test_check_count(source_data, target_data, data_quality_library):
     data_quality_library.check_count(source_data, target_data)
 
 @pytest.mark.parquet_data
+@pytest.mark.data_quality
 @pytest.mark.facility_name_min_time_spent_per_visit_date
 def test_check_uniqueness(target_data, data_quality_library):
     data_quality_library.check_duplicates(target_data)
 
 @pytest.mark.parquet_data
+@pytest.mark.data_quality
 @pytest.mark.facility_name_min_time_spent_per_visit_date
 def test_check_not_null_values(target_data, data_quality_library):
     data_quality_library.check_not_null_values(target_data, ['facility_name', 'visit_date', 'min_time_spent'])
--- PyTestDQFramework/tests/test_examples.py
+++ PyTestDQFramework/tests/test_examples.py
@@ -7,27 +7,40 @@ Author(s): Name Surname
 import pytest
 
 
-@pytest.fixture(scope='module')
-def source_data(db_connection):
-    source_query = """
-    SELECT ...
-    """
-    source_data  = db_connection.get_data_sql(source_query)
-    return source_data
+# @pytest.fixture(scope='module')
+# def source_data(db_connection):
+#     source_query = """
+#     SELECT
+#         f.facility_name,
+#         CAST(v.visit_timestamp AS DATE) AS visit_date,
+#         MIN(v.duration_minutes) AS avg_time_spent
+#     FROM visits v
+#     JOIN facilities f
+#         ON f.id = v.facility_id
+#     WHERE v.duration_minutes IS NOT NULL
+#     GROUP BY
+#         f.facility_name,
+#         CAST(v.visit_timestamp AS DATE)
+#     ORDER BY
+#         f.facility_name,
+#         visit_date;
+#     """
+#     source_data  = db_connection.get_data_sql(source_query)
+#     return source_data
 
 
-@pytest.fixture(scope='module')
-def target_data(parquet_reader):
-    target_path = '/root/path/to/file'
-    target_data = parquet_reader.process(target_path)
-    return target_data
+# @pytest.fixture(scope='module')
+# def target_data(parquet_reader):
+#     target_path = '/parquet_data/facility_name_min_time_spent_per_visit_date/'
+#     target_data = parquet_reader.process(target_path)
+#     return target_data
 
 
-@pytest.mark.example
-def test_check_dataset_is_not_empty(target_data, data_quality_library):
-    data_quality_library.check_dataset_is_not_empty(target_data)
+# @pytest.mark.example
+# def test_check_dataset_is_not_empty(target_data, data_quality_library):
+#     data_quality_library.check_dataset_is_not_empty(target_data)
 
 
-@pytest.mark.example
-def test_check_count(source_data, target_data, data_quality_library):
-    data_quality_library.check_count(source_data, target_data)
+# @pytest.mark.example
+# def test_check_count(source_data, target_data, data_quality_library):
+#     data_quality_library.check_count(source_data, target_data)
